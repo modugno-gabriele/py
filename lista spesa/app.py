0a9595664a965bf0ec286cbe3b7b274b689f467e
modugno-gabriele <modugno.gabriele@iisgalvanimi.edu.it>
2025-04-04 11:00:48 +0000

    esercizio py

--- lista spesa/app.py
+++ lista spesa/app.py
@@ -1,9 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for
 app = Flask(__name__)
 x = "ciao"
-lista = ["pane","ramen","cipolla","gatto","cane"]
-@app.route('/aggiungi',methods=["POST"])
+lista = []
+@app.route('/')
 def home():
  return render_template("index.html",lista = lista)
+
+
+
+
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+  elemento = request.form['elemento']
+  if elemento:
+    lista.append(elemento)
+    return redirect(url_for('home'))
+  
+@app.route('/rimuovi/<int:indice>', methods=['POST'])
+def rimuovi(indice):
+  if 0 <= indice < len(lista):
+    lista.pop(indice)
+    return redirect(url_for('home'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
   app.run(debug=True)
